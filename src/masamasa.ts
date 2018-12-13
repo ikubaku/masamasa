e5f23367f5099f4bb25186e88ae1ba65a8d0efad
ikubaku <hide4d51@gmail.com>
2018-12-13 13:39:19 +0900

    Refactor src/masamasa.ts

--- src/masamasa.ts
+++ src/masamasa.ts
@@ -1,58 +1,58 @@
-import * as p5 from "p5"
+import * as p5 from "p5";
 
 function s(ctx: p5) {
     let fps: number;
-    let is_fdrop: boolean;
-    let d_time: number;
-    let ftime_s: number;
-    let ftime_e: number;
-    
+    let isFrameDropped: boolean;
+    let deltaTime: number;
+    let frameStartTime: number;
+    let frameEndTime: number;
+
     ctx.setup = () => {
-	// TODO: Implement the way to specify the frame rate
-	fps = 30;
+        // TODO: Implement the way to specify the frame rate
+        fps = 30;
         // TODO: Implement the way to specify the canvas size
         ctx.createCanvas(800, 600);
         ctx.frameRate(fps);
 
-	// ftime_s is negative if it is the first time to enter the draw() loop
-	ftime_s = -1;
+        // frameStartTime is negative if it is the first time to enter the draw() loop
+        frameStartTime = -1;
     };
 
     ctx.draw = () => {
-        // if ftime_s < 0 it is the first time to do draw()
-        if(ftime_s >= 0) {
+        // if frameStartTime < 0 it is the first time to do draw()
+        if (frameStartTime >= 0) {
             // Get frame end time
-            ftime_e = ctx.millis();
+            frameEndTime = ctx.millis();
             // Update delta time
-            d_time = (ftime_e - ftime_s) / 1000;
-            if(d_time <= 0) {
+            deltaTime = (frameEndTime - frameStartTime) / 1000;
+            if (deltaTime <= 0) {
                 // Frame drop
-                d_time = 0;
-                is_fdrop = true;
+                deltaTime = 0;
+                isFrameDropped = true;
             } else {
-                is_fdrop = false;
-	    }
+                isFrameDropped = false;
+            }
         }
-        
+
         // Get frame start time
-        ftime_s = ctx.millis();
+        frameStartTime = ctx.millis();
         // Clear canvas
         ctx.background(0);
 
         // Start update process
-        
+
         // Draw process
-	// // Sample object
+        // // Sample object
         ctx.fill(255);
         ctx.rect(390, 290 + 20 * ctx.sin(ctx.TWO_PI * ctx.frameCount / 30), 20, 20);
 
-	// Delta time viewer(debug)
-	if(is_fdrop) {
-	    ctx.fill(ctx.color(255, 0, 0));
-	} else {
-	    ctx.fill(255);
-	}
-	ctx.text("d_time= " + ctx.str(d_time) + " (s)", 10, 10);
+        // Delta time viewer(debug)
+        if (isFrameDropped) {
+            ctx.fill(ctx.color(255, 0, 0));
+        } else {
+            ctx.fill(255);
+        }
+        ctx.text("deltaTime= " + ctx.str(deltaTime) + " (s)", 10, 10);
     };
 }
 
